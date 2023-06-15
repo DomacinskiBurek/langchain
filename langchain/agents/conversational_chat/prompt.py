0673be7c8f3c05cec19ccc92ec04edc96b1ceb8b
Bojan Vukajlovic <b.vukajlovic@gigaaa.com>
2023-06-15 09:08:49 +0200

    minor changes in promp

--- langchain/agents/conversational_chat/prompt.py
+++ langchain/agents/conversational_chat/prompt.py
@@ -22,8 +22,7 @@ Markdown code snippet formatted in the following schema:
 
 
 **Option #2:**
-Use this to ask the user a clarification question if the question is not a follow-up question, clarification or specification and if the user's question is not clear enough or you need more information to provide an answer.
-Do not use if you have good enough answer on the user's question.
+Use this to ask the user a clarification question if you need more information to provide an answer.
 Markdown code snippet formatted in the following schema:
 
 ```json
@@ -34,7 +33,7 @@ Markdown code snippet formatted in the following schema:
 
 
 **Option #3**
-Use this if user's question is small talk (an informal type of discourse that does not cover any functional topics of conversation or any transactions that need to be addressed) or if you have a direct response or a good enough answer to the user's question.
+Use this when user's question is small talk (an informal type of discourse that does not cover any functional topics of conversation or any transactions that need to be addressed) and when you have a direct response and when you have a good enough answer to the user's question.
 Markdown code snippet formatted in the following schema:
 
 ```json
@@ -49,7 +48,7 @@ SUFFIX = """TOOLS
 ------
 
 Assistant can use tools to look up information that may be helpful in answering the users original question.
-Primarily take tools that, according to their name and description, are intended for a certain area in question. If the answer is not precise, good enough or does not give an adequate answer at all, consider using other tools even though they may not be intended for the area in question.
+When the answer is not precise, good enough or does not give an adequate answer at all, consider using other tools even though they may not be intended for the area in question.
 While using tools, do not provide same question to the tool.
 The tools the Assistant can use are:
 
@@ -60,21 +59,12 @@ USER'S INPUT
 
 {format_instructions} 
 
-Consider what the user is asking for in the question. Here's an example:
-Q: What is the capital of Japan?
-A: The capital of Japan is Tokyo?
-Q: How many citizens lives in capital?
-Thought: What's capital? Ahaa, capital is Tokyo?
-A: ?
+Consider the conversation history as an additional source of information to look up for information if question serves as a follow-up question, clarification, or specification. 
 
-Consider the conversation history provided below and determine whether the user's question 
-serves as a follow-up question, clarification, or specification. 
-
-It is very important to answer every question whether it is related to the topic or not!
+It is very important to answer every question even if it isn't related to the topic!
 Do not forget to follow given rules!
 
-IMPORTANT: make sure while responding to respond with a markdown code snippet of a json blob with a single action, markdown code snippet must be correct snippet as it's provided and NOTHING else!
-Here is the user's input:
+Here is the user's input (Remember to respond with a markdown code snippet of a json blob with a single action, and NOTHING else):
 
 {{{{input}}}}"""
 
@@ -87,6 +77,4 @@ Use provided observation to answer user's question:
 USER'S INPUT
 --------------------
 
-
-IMPORTANT: make sure while responding to respond with a markdown code snippet of a json blob with a single action, markdown code snippet must be correct snippet as it's provided and NOTHING else!
-Okay, so what is the response to my last comment? If using information obtained from the tools you must mention it explicitly without mentioning the tool names - I have forgotten all TOOL RESPONSES!"""
+Okay, so what is the response to my last comment? If using information obtained from the tools you must mention it explicitly without mentioning the tool names - I have forgotten all TOOL RESPONSES! Remember to respond with a markdown code snippet of a json blob with a single action, and NOTHING else!"""
